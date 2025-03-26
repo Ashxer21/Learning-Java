1404a338807e90d7819689e0c819c5139c2eec80
Ashxer <taras,firhuk72@gmail.com>
2025-03-26 02:04:30 +0200

    Clean Branch

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Store store = new Store();
-
-        store.addProducts(new Product("Bread", 20.4,100));
-        store.addProducts(new Product("Milk", 30.10,50));
-
-        store.showAllProducts();
-
-        store.buyProducts("Bread", 3);
-        store.buyProducts("Milk", 50);
-
-        store.showAllProducts();
 
 
     }
